903c60809a453b999973b6ea758f6dacd9973674
Jérémie Bernard <jeremie.bernard.1@ulaval.ca>
2024-05-31 12:21:10 -0400

    PRD-34: add django base and postgres

--- docker/ohif/main.py
+++ docker/ohif/main.py
@@ -1,5 +1,11 @@
 from pyorthanc import Orthanc, find_patients
 
+#TODO add anonymisation via python
+#TODO add study via python
+#TODO add queue for anonymise -> using django and celery -> queue the anonymise process async and store json config for log
+#TODO apply conversions if needed to dicom
+#TODO review existing solutions for each part with Azure -> ce qui existe pour Azure est clé en main, mais n'utilise pas ORTHANC, vaut mieux l'apprendre par nous-même
+
 def main():
     orthanc = Orthanc(url='http://localhost:8053/', username='orthanc', password='orthanc')
     patients = find_patients(
